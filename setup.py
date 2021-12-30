37e141baeda00354accdb000db9d7e046eaa713f
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2021-12-30 11:22:09 -0300

    add versioning method

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+version_ns = {}
+with open(os.path.join(here, '_version.py')) as f:
+    exec(f.read(), {}, version_ns)
 
 setup(
     name='gee-extraction',
-    version='0.0.1',
+    version=version_ns['__version__'],
     py_modules=['gee-extraction'],
     install_requires=[
         'Click',
